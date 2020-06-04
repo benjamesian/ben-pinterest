417244abfd1fb783ec4c35906104723f44e1be7d
Ben Cook <808@holbertonschool.com>
2020-06-04 14:07:00 -0400

    Back4App React bootstrap

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Nav from "./Nav";
+import Home from "./home/Home";
+import Register from "./register/Register";
+import * as Env from "./environments";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Parse from "parse";
+
+Parse.initialize(Env.APPLICATION_ID, Env.JAVASCRIPT_KEY);
+Parse.serverURL = Env.SERVER_URL;
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/register" exact component={Register}></Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/environments.js
@@ -0,0 +1,5 @@
+module.exports = {
+    APPLICATION_ID: 'ZQGNNmalngJw6qhM2b2L0PpTuwiZsyB29gFPDguQ',
+    JAVASCRIPT_KEY: 'syS9DGFefozheAJ5n4R161rbWS3gKSyQZWXxCL6z',
+    SERVER_URL: 'https://parseapi.back4app.com/'
+}
